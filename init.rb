50e66f3d032bc9330ca350f156896882d64763e3
y-yoshinoya <y-yoshinoya@ase.co.jp>
2016-03-01 16:24:25 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_project_overview_customize do
+  name 'Redmine Project Overview Customize plugin'
+  author 'Yuki Yoshinoya  '
+  description 'Customize for project overview'
+  version '0.0.1'
+  url 'https://github.com/y-yoshinoya/redmine_project_overview_customize'
+  author_url 'https://github.com/y-yoshinoya/'
+end
